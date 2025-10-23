5de148f89c04f0abd3a6da9020485fc4a99e5a63
ColinBertin <colinbertin12@gmail.com>
2025-10-23 21:24:46 +0900

    fix position btns

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className="h-full flex flex-col justify-center items-center gap-4">
       <h1>My Spending</h1>
-      <div className="flex justify-center gap-4">
+      <div className="flex flex-col md:flex-row justify-center gap-4">
         <Link
           className="bg-gray-700 hover:bg-gray-500 text-white text-center font-semibold py-2 px-4 rounded-3xl self-center w-56 sm:w-36"
           href="/login"
