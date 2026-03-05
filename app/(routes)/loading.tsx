51f4ea8cb2b3a88bec3f7a91bb4a3a86938d7086
ColinBertin <colinbertin12@gmail.com>
2026-03-05 13:14:00 +0900

    add missing loading page

---
+++ app/(routes)/loading.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Spinner from "@/components/Spinner";
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-[calc(100vh-8rem)] w-full items-center justify-center">
+      <Spinner />
+    </div>
+  );
+}
